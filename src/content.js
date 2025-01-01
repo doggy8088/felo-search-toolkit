8f0cbfe9da62a84d91c39197b68e124157b3babc
Will <doggy.huang@gmail.com>
2025-01-02 01:22:54 +0800

    0.9.0 (2025/01/02)
      - 避免與 `Ctrl+C` 衝突

--- src/content.js
+++ src/content.js
@@ -1,14 +1,14 @@
 (async function () {
     'use strict';
 
-    document.addEventListener("keydown", async (event) => {
+    document.addEventListener('keydown', async (event) => {
 
         // 從網址列取得 pathinfo
         const currentPath = window.location.pathname;
 
-        if (event.key === "j") {
+        if (!event.ctrlKey && event.key === 'j') {
             // 如果是輸入欄位，就不要觸發。但是按下 alt+j 就可以觸發這個功能。
-            if ((event.target.tagName === "INPUT" || event.target.tagName === "TEXTAREA") && !event.altKey) {
+            if ((event.target.tagName === 'INPUT' || event.target.tagName === 'TEXTAREA') && !event.altKey) {
                 return;
             }
 
@@ -16,7 +16,7 @@
             const currentUrl = window.location.pathname;
 
             // 2. 找到和目前 pathinfo 一樣的超連結
-            const matchingLink = document.querySelector(`a[href="${currentUrl}"]`);
+            const matchingLink = document.querySelector(`a[href='${currentUrl}']`);
             if (matchingLink) {
                 // 找到下一個 link
                 const nextLink = matchingLink?.closest('li')?.nextElementSibling?.querySelector('a')
@@ -26,15 +26,15 @@
                     nextLink?.click();
                 }
             } else {
-                const firstLink = document.querySelector(`a[href*="/search/"]`);
+                const firstLink = document.querySelector(`a[href*='/search/']`);
                 firstLink?.click();
             }
             event.preventDefault();
         }
 
-        if (event.key === "k") {
+        if (!event.ctrlKey && event.key === 'k') {
             // 如果是輸入欄位，就不要觸發。但是按下 alt+k 就可以觸發這個功能。
-            if ((event.target.tagName === "INPUT" || event.target.tagName === "TEXTAREA") && !event.altKey) {
+            if ((event.target.tagName === 'INPUT' || event.target.tagName === 'TEXTAREA') && !event.altKey) {
                 return;
             }
 
@@ -42,7 +42,7 @@
             const currentUrl = window.location.pathname;
 
             // 2. 找到和目前 pathinfo 一樣的超連結
-            const matchingLink = document.querySelector(`a[href="${currentUrl}"]`);
+            const matchingLink = document.querySelector(`a[href='${currentUrl}']`);
             const previousLink = matchingLink?.closest('li')?.previousElementSibling?.querySelector('a')
                 ?? matchingLink?.closest('li')?.previousElementSibling?.previousElementSibling?.querySelector('a');
             if (previousLink) {
@@ -53,69 +53,69 @@
         }
 
         // 按下 Alt+/ 就先找出所有 button 元素，比對元素內容，如果為「歷史紀錄」就點擊它
-        if (event.key === "/") {
+        if (!event.ctrlKey && event.key === '/') {
             // 如果是輸入欄位，就不要觸發。但是按下 alt+/ 就可以觸發這個功能。
-            if ((event.target.tagName === "INPUT" || event.target.tagName === "TEXTAREA") && !event.altKey) {
+            if ((event.target.tagName === 'INPUT' || event.target.tagName === 'TEXTAREA') && !event.altKey) {
                 return;
             }
 
-            clickButtonByText(["歷史記錄", "历史记录", "履歴記録", "History"]);
+            clickButtonByText(['歷史記錄', '历史记录', '履歴記録', 'History']);
             event.preventDefault();
         }
 
         // 按下 Alt+t 就先找出所有 button 元素，比對元素內容，如果為「主題集」就點擊它
-        if (event.key === "t") {
+        if (!event.ctrlKey && event.key === 't') {
             // 如果是輸入欄位，就不要觸發。但是按下 alt+t 就可以觸發這個功能。
-            if ((event.target.tagName === "INPUT" || event.target.tagName === "TEXTAREA") && !event.altKey) {
+            if ((event.target.tagName === 'INPUT' || event.target.tagName === 'TEXTAREA') && !event.altKey) {
                 return;
             }
 
-            clickButtonByText(["主題集", "主题集", "トピック集", "Topic Collections"]);
+            clickButtonByText(['主題集', '主题集', 'トピック集', 'Topic Collections']);
             event.preventDefault();
         }
 
         // 按下 Alt+s 就先找出所有 button 元素，比對元素內容，如果為「分享」就點擊它
-        if (event.key === "s") {
+        if (!event.ctrlKey && event.key === 's') {
             // 如果是輸入欄位，就不要觸發。但是按下 alt+s 就可以觸發這個功能。
-            if ((event.target.tagName === "INPUT" || event.target.tagName === "TEXTAREA") && !event.altKey) {
+            if ((event.target.tagName === 'INPUT' || event.target.tagName === 'TEXTAREA') && !event.altKey) {
                 return;
             }
 
-            clickButtonByText(["分享", "分享", "共有する", "Share"]);
+            clickButtonByText(['分享', '分享', '共有する', 'Share']);
             event.preventDefault();
         }
 
         // 按下 Alt+c 就先找出所有包含 [tabindex] 屬性的元素，比對元素內容，如果為「建立主題」就點擊它
-        if (event.key === "c") {
+        if (!event.ctrlKey && event.key === 'c') {
             // 如果是輸入欄位，就不要觸發。但是按下 alt+c 就可以觸發這個功能。
-            if ((event.target.tagName === "INPUT" || event.target.tagName === "TEXTAREA") && !event.altKey) {
+            if ((event.target.tagName === 'INPUT' || event.target.tagName === 'TEXTAREA') && !event.altKey) {
                 return;
             }
 
-            clickButtonByText(["建立主題", "建立主题", "トピックを作成", "Create topic"]);
+            clickButtonByText(['建立主題', '建立主题', 'トピックを作成', 'Create topic']);
             event.preventDefault();
         }
 
         // 按下 Ctrl+Delete 快速刪除 Felo Search 聊天記錄
-        if (event.ctrlKey && event.key === "Delete") {
+        if (event.ctrlKey && event.key === 'Delete') {
             // 如果是輸入欄位，就不要觸發
-            if (event.target.tagName === "INPUT" || event.target.tagName === "TEXTAREA") {
+            if (event.target.tagName === 'INPUT' || event.target.tagName === 'TEXTAREA') {
                 return;
             }
 
             // 找到和目前 pathinfo 一樣的超連結
-            const matchingLink = document.querySelector(`a[href="${currentPath}"]`);
+            const matchingLink = document.querySelector(`a[href='${currentPath}']`);
             const nextLink = matchingLink?.closest('li')?.nextElementSibling?.querySelector('a')
                 ?? matchingLink?.closest('li')?.nextElementSibling?.nextElementSibling?.querySelector('a');
 
             // 找到超連結的下一個同層的 section 元素
             const nextSection = matchingLink?.nextElementSibling;
             // 找到下一層的 button 按鈕
-            const button = nextSection?.querySelector("button");
+            const button = nextSection?.querySelector('button');
             button?.click();
 
             await delay(200); // 加入一點延遲來模擬真實打字過程
-            clickButtonByText(["確認", "确认", "確認", "Confirm"]);
+            clickButtonByText(['確認', '确认', '確認', 'Confirm']);
 
             await delay(200);
 
@@ -129,7 +129,7 @@
         }
 
         // 按下 Ctrl+B 快速切換側邊欄
-        if (event.ctrlKey && event.key === "b") {
+        if (event.ctrlKey && event.key === 'b') {
             // 找到 section 的 class 為「cursor-pointer」的元素
             const svg = document.querySelector('section.cursor-pointer svg');
             svg?.parentElement.click();
@@ -137,15 +137,15 @@
         }
 
         // 按下 Escape 就點擊 document.querySelector('img').click()
-        if (event.key === "Escape") {
-            // 如果有 [role="dialog"] 就不要觸發
+        if (!event.ctrlKey && event.key === 'Escape') {
+            // 如果有 [role='dialog'] 就不要觸發
             if (document.querySelector('[role="dialog"]')) {
                 return;
             }
 
             // 如果是輸入欄位，就不要觸發
-            if (event.target.tagName === "INPUT" || event.target.tagName === "TEXTAREA") {
-                if (event.target.value === '' && currentPath.includes("/history")) {
+            if (event.target.tagName === 'INPUT' || event.target.tagName === 'TEXTAREA') {
+                if (event.target.value === '' && currentPath.includes('/history')) {
                     // 只有在歷史紀錄頁面且搜尋欄位是空白時才會觸發
                     window.history.back();
                 }
@@ -183,7 +183,7 @@
 
         for (let i = 0; i < tabindexElements.length; i++) {
             const tabindexElement = tabindexElements[i];
-            if (tabindexElement.tagName !== "BUTTON" && tabindexElement.tabIndex != -1) {
+            if (tabindexElement.tagName !== 'BUTTON' && tabindexElement.tabIndex != -1) {
                 buttons = [...buttons, tabindexElement];
             }
         }
@@ -240,18 +240,18 @@
             let altKeyTimeout;
             let altKeyIsOn = false;
             document.addEventListener('keydown', (event) => {
-                if (event.key === "Alt" && altKeyIsOn == false) {
+                if (event.key === 'Alt' && altKeyIsOn == false) {
                     altKeyIsOn = true;
                     altKeyTimeout = setTimeout(() => {
-                        showHotkeyHintForButtonByText(["主題集", "主题集", "トピック集", "Topic Collections"], 'Alt+T');
-                        showHotkeyHintForButtonByText(["歷史記錄", "历史记录", "履歴記録", "History"], 'Alt+/');
-                        showHotkeyHintForButtonByText(["分享", "分享", "共有する", "Share"], 'Alt+S');
+                        showHotkeyHintForButtonByText(['主題集', '主题集', 'トピック集', 'Topic Collections'], 'Alt+T');
+                        showHotkeyHintForButtonByText(['歷史記錄', '历史记录', '履歴記録', 'History'], 'Alt+/');
+                        showHotkeyHintForButtonByText(['分享', '分享', '共有する', 'Share'], 'Alt+S');
                     }, 1000); // 1 秒
                 }
             });
             document.addEventListener('keyup', (event) => {
                 altKeyIsOn = false;
-                if (event.key === "Alt") {
+                if (event.key === 'Alt') {
                     // 删除所有 span.badge
                     document.querySelectorAll('span.badge').forEach((el) => el.remove());
                     altKeyTimeout = clearTimeout(altKeyTimeout);
@@ -260,7 +260,7 @@
         }
     }
 
-    // SPA 网页不应该要有 type="submit" 的按钮，这会导致键盘的 Escape 无法被触发！
+    // SPA 网页不应该要有 type='submit' 的按钮，这会导致键盘的 Escape 无法被触发！
     // setInterval(() => {
     //     let backdropBlur = document.querySelector('div.backdrop-blur-md');
     //     let btn = backdropBlur?.parentElement?.querySelector('button');
